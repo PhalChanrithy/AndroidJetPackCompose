b090a99586f931805ac379138a3489fb5e8b5848
PhalChanrithy <phalrithy@yahoo.com>
2026-01-26 11:53:16 +0700

    Changes

--- dessert-clicker/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ dessert-clicker/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -16,7 +16,13 @@
 
 package com.example.dessertclicker.model
 
+import androidx.annotation.DrawableRes
+
 /**
  * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
  */
-data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+data class Dessert(
+    @DrawableRes val imageId: Int,
+    val price: Int,
+    val startProductionAmount: Int
+)
